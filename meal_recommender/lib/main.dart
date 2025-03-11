55e8cad866325401cacb52c043b4b04b58ad6ff7
mohamed ahmed <m.ahmed20201863@gmail.com>
2025-03-11 21:06:52 +0200

    Set Bloc observer for state management in main application entry

--- meal_recommender/lib/main.dart
+++ meal_recommender/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:meal_recommender/core/routes/app_router.dart';
 import 'package:meal_recommender/core/themes/application_theme_manager.dart';
 import 'core/routes/app_views.dart';
+import 'core/services/cubit_observer.dart';
 
 void main() {
+  Bloc.observer = AppCubitObserver();
   runApp(const MyApp());
 }
 
