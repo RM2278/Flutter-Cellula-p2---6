793c676419b05ebe0b7039b7c19a501959a2e134
mohamed ahmed <m.ahmed20201863@gmail.com>
2025-03-24 21:45:45 +0200

    add dish model and related entities for meal recommendation feature; implement JSON serialization and deserialization

---
+++ meal_recommender/lib/features/ai/data/models/dish_model.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+
+import '../../domain/entities/dish_entity.dart';
+
+class DishModel extends Dish {
+  DishModel({
+    required super.name,
+    required super.summary,
+    required super.typeOfMeal,
+    required super.time,
+    required super.imageUrl,
+    required List<IngredientModel> super.ingredients,
+    required NutritionModel super.nutrition,
+    required DirectionsModel super.directions,
+  });
+
+  factory DishModel.fromJson(Map<String, dynamic> json) {
+    return DishModel(
+      name: json['name'],
+      summary: json['summary'],
+      typeOfMeal: json['typeOfMeal'],
+      time: json['time'],
+      imageUrl: json['imageUrl'],
+      ingredients: (json['ingredients'] as List)
+          .map((i) => IngredientModel.fromJson(i))
+          .toList(),
+      nutrition: NutritionModel.fromJson(json['nutrition']),
+      directions: DirectionsModel.fromJson(json['directions']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'summary': summary,
+      'typeOfMeal': typeOfMeal,
+      'time': time,
+      'imageUrl': imageUrl,
+      'ingredients': ingredients.map((i) => (i as IngredientModel).toJson()).toList(),
+      'nutrition': (nutrition as NutritionModel).toJson(),
+      'directions': (directions as DirectionsModel).toJson(),
+    };
+  }
+}
+
+class IngredientModel extends Ingredient {
+  IngredientModel({
+    required super.name,
+    required super.quantity,
+    required super.unit,
+    required super.imageUrl,
+  });
+
+  factory IngredientModel.fromJson(Map<String, dynamic> json) {
+    return IngredientModel(
+      name: json['name'],
+      quantity: json['quantity'],
+      unit: json['unit'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'unit': unit,
+      'imageUrl': imageUrl,
+    };
+  }
+}
+
+class NutritionModel extends Nutrition {
+  NutritionModel({
+    required super.calories,
+    required super.protein,
+    required super.carbs,
+    required super.fat,
+    required super.vitamins,
+  });
+
+  factory NutritionModel.fromJson(Map<String, dynamic> json) {
+    return NutritionModel(
+      calories: json['calories'],
+      protein: json['protein'],
+      carbs: json['carbs'],
+      fat: json['fat'],
+      vitamins: List<String>.from(json['vitamins']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'calories': calories,
+      'protein': protein,
+      'carbs': carbs,
+      'fat': fat,
+      'vitamins': vitamins,
+    };
+  }
+}
+
+class DirectionsModel extends Directions {
+  DirectionsModel({
+    required super.firstStep,
+    required super.secondStep,
+    required super.additionalSteps,
+  });
+
+  factory DirectionsModel.fromJson(Map<String, dynamic> json) {
+    return DirectionsModel(
+      firstStep: json['firstStep'],
+      secondStep: json['secondStep'],
+      additionalSteps: List<String>.from(json['additionalSteps']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'firstStep': firstStep,
+      'secondStep': secondStep,
+      'additionalSteps': additionalSteps,
+    };
+  }
+}
---
+++ meal_recommender/lib/features/ai/domain/entities/dish_entity.dart
@@ -0,0 +1,63 @@
+class Dish {
+  final String name;
+  final String summary;
+  final String typeOfMeal;
+  final int time;
+  final String imageUrl;
+  final List<Ingredient> ingredients;
+  final Nutrition nutrition;
+  final Directions directions;
+
+  Dish({
+    required this.name,
+    required this.summary,
+    required this.typeOfMeal,
+    required this.time,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.nutrition,
+    required this.directions,
+  });
+}
+
+class Ingredient {
+  final String name;
+  final String quantity;
+  final String unit;
+  final String imageUrl;
+
+  Ingredient({
+    required this.name,
+    required this.quantity,
+    required this.unit,
+    required this.imageUrl,
+  });
+}
+
+class Nutrition {
+  final String calories;
+  final String protein;
+  final String carbs;
+  final String fat;
+  final List<String> vitamins;
+
+  Nutrition({
+    required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fat,
+    required this.vitamins,
+  });
+}
+
+class Directions {
+  final String firstStep;
+  final String secondStep;
+  final List<String> additionalSteps;
+
+  Directions({
+    required this.firstStep,
+    required this.secondStep,
+    required this.additionalSteps,
+  });
+}
