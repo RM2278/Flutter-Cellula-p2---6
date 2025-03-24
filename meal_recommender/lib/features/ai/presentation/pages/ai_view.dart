698624d7984d074886eb9f146a60f1f10b0a647d
mohamed ahmed <m.ahmed20201863@gmail.com>
2025-03-24 23:11:07 +0200

    add AiView widget for displaying AI recommendations

---
+++ meal_recommender/lib/features/ai/presentation/pages/ai_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AiView extends StatelessWidget {
+  const AiView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('AI View'),
+      ),
+    );
+  }
+}
